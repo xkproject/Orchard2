e95f8da6092601d3852d4a907165038fb4b43f19
jersiovic <jersio@hotmail.com>
2021-02-04 13:58:58 +0100

    added our private repo y changed version for npkg packages

--- GenerateNugetPackages.ps1
+++
@@ -1,6 +0,0 @@
-nuget.exe sources Remove -Name "KeTePongoOrchardCore" 
-nuget.exe sources Add -Name "KeTePongoOrchardCore" -Source "https://pkgs.dev.azure.com/ketepongo/KTP/_packaging/KeTePongoOrchardCore/nuget/v3/index.json" -username jersio@hotmail.com -password t4p76c7idjjktllwriofrzpc4v4xth7uaejx7ibwif4kl246n4sq
-dotnet pack
-Get-ChildItem -Recurse -File *.nupkg | Foreach {dotnet nuget push --source "KeTePongoOrchardCore" --api-key KeTePongo $_.FullName }
-
-
